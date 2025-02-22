4baa1f10e8555c0376abc42d149b035aa9c53898
mianmoawaz <mianmoawaz0@gmail.com>
2025-02-22 01:57:10 -0800

    message

---
+++ lib/model/todo_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoModel extends StatefulWidget {
+  const TodoModel({super.key});
+
+  @override
+  State<TodoModel> createState() => _TodoModelState();
+}
+
+class _TodoModelState extends State<TodoModel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/model/user_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserModel extends StatefulWidget {
+  const UserModel({super.key});
+
+  @override
+  State<UserModel> createState() => _UserModelState();
+}
+
+class _UserModelState extends State<UserModel> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
